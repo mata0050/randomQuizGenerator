d81ea137029e2bf92b33fed3af0622fc7f1842ad
Fathima thasnim <thasnimfathima492@gmail.com>
2022-02-14 12:54:57 -0500

    React-router setup for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+
+import Home from './Components/HomePage/Home';
+import User from './Components/User/User';
+import Quiz from './Components/Quiz/Quiz';
+import Score from './Components/FinalScore/Score';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="user">User</Link>
+        <Link to="quiz">Quiz</Link>
+        <Link to="score">Score</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="user" element={<User/>}></Route>
+        <Route path="quiz" element={<Quiz/>}></Route>
+        <Route path="score" element={<Score/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
