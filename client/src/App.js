522437b749236203e7804ce6206f1d99d86094ff
Fathima thasnim <thasnimfathima492@gmail.com>
2022-02-14 21:39:22 -0500

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './Components/HomePage/Home';
 import User from './Components/User/User';
 import Quiz from './Components/Quiz/Quiz';
 import Score from './Components/FinalScore/Score';
+import Header from './Components/Header/Header';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
 
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Link to="/">Home</Link>
-        <Link to="user">User</Link>
-        <Link to="quiz">Quiz</Link>
-        <Link to="score">Score</Link>
+        <Header/>
       </div>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="user" element={<User/>}></Route>
         <Route path="quiz" element={<Quiz/>}></Route>
         <Route path="score" element={<Score/>}></Route>
+        <Route path="login" element={<Login/>}></Route>
+        <Route path="signup" element={<Signup/>}></Route>
       </Routes>
     </BrowserRouter>
     
---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+ import './Header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+      
+      <h2>LOGO</h2>
+      <div className='menu'> 
+      <NavLink to="/">
+        <span>Home</span>
+      </NavLink>
+      <NavLink to="user">
+        <span>User</span>
+      </NavLink>
+      <NavLink to="quiz">
+        <span>Quiz</span>
+      </NavLink>
+      <NavLink to="score">
+        <span>Score</span>
+      </NavLink>
+     
+      <NavLink to="login">
+        <span className='button'>Login</span>
+      </NavLink>
+      <NavLink to="signup">
+        <span className='button'> Signup</span>
+      </NavLink>
+     
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default Header
