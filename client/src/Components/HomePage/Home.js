cd4c5c4bd01f98735afd0ccc9c8623bdb66ffda4
Fathima thasnim <thasnimfathima492@gmail.com>
2022-02-15 19:49:53 -0500

    add styles to home and user page

--- client/src/Components/HomePage/Home.js
+++ client/src/Components/HomePage/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+// import img1 from '/Users/user/lighthouse/final-project/client/src/images/quizimg.jpeg';
+// import img2 from '/Users/user/lighthouse/final-project/client/src/images/quizimg2.jpeg';
+import './Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='flex-item quiz'>
+      <div className='item1'><h1>Quizizz</h1></div>
+      <div className='item2'><h5>Take quiz to know your knowledge</h5></div>
+      </div>
+      
+      
+    </div>
   )
 }
 
--- client/src/Components/Quiz/Quiz.js
+++ client/src/Components/Quiz/Quiz.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import './Quiz.css';
 
 function Quiz() {
   return (
-    <div>Quiz</div>
-  )
+    // <div className="container">
+    //   <div>
+    //     <div class="row justify-content-evenly">
+    //       <div class="col-4">option A</div>
+    //       <div class="col-4">option B</div>
+    //     </div>
+    //     <div class="row justify-content-evenly">
+    //       <div class="col-4">option C</div>
+    //       <div class="col-4">option D</div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div class="container">
+  <div class="row row-cols-2" >
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+</div>
+  );
 }
 
-export default Quiz
+export default Quiz;
