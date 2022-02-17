99845eb9b262c46021114e623a1eb8ff26b67324
mata0050 <mata0050@outlook.com>
2022-02-17 14:21:01 -0500

    merge conflict in Login, SignUp Component fixed

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import Alert from "../ErrorMessage/Alert";
-import { Navigate } from "react-router-dom";
-import './Login.css';
-
-// utils
-import api from "../../utils/api";
-=======
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// CSS
+import './Login.css';
+
 // api call
 import loginUser from '../../api/loginUser';
->>>>>>> d445ebef94cfb0d997fc4b007b4c3395fc5244b7
 
 function Login({ setUserToken }) {
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
   const { email, password } = formData;
   const navigate = useNavigate();
@@ -30,30 +23,7 @@ function Login({ setUserToken }) {
   // Login a User
   const onSubmit = (e) => {
     e.preventDefault();
-<<<<<<< HEAD
-    try {
-      const { data } = await api.post("/auth", formData);
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("userProfile", JSON.stringify(data.userProfile));
-      setUserToken(data.token);
-      setRedirect(true);
-    } catch (error) {
-      setError(true);
-    }
-  };
 
-  // Redirect after login to quiz page
-  if (redirect) {
-    return <Navigate to="/quiz" />;
-  }
-  return (
-    <div className="card">
-      {error && (
-        <Alert msg={"Login Error Please Try Again"} alertType={"danger"} />
-      )}
-      <div className="card-header"><h3>Login</h3></div>
-      <div className="card-body">
-=======
     loginUser(setUserToken, formData);
   };
 
@@ -61,38 +31,40 @@ function Login({ setUserToken }) {
     navigate('/quiz');
   }
   return (
-    <>
-      <div>Login</div>
->>>>>>> d445ebef94cfb0d997fc4b007b4c3395fc5244b7
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="email" >
-            Email
-          </label>
-          <input className="form-control"
-            type="text"
-            value={email}
-            id="email"
-            name="email"
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password" >
-            Password
-          </label>
-          <input className="form-control"
-            type="password"
-            value={password}
-            id="password"
-            name="password"
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-        <button type="submit" className='btn btn-primary start-btn'>Login</button>
-        </div>
-      </form>
+    <div className='card'>
+      <div className='card-header'>
+        <h3>Login</h3>
+      </div>
+      <div className='card-body'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              className='form-control'
+              type='text'
+              value={email}
+              id='email'
+              name='email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              className='form-control'
+              type='password'
+              value={password}
+              id='password'
+              name='password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-primary start-btn'>
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/Components/Signup/Signup.js
+++ client/src/Components/Signup/Signup.js
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import Alert from "../ErrorMessage/Alert";
-import { Navigate } from "react-router-dom";
-import "./Signup.css";
-
-// utils
-import api from "../../utils/api";
-=======
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+import './Signup.css';
+
 // api call
 import signUpUser from '../../api/signUpUser';
->>>>>>> d445ebef94cfb0d997fc4b007b4c3395fc5244b7
 
 function Signup({ setUserToken }) {
   const [formData, setFormData] = useState({
-    first_name: "",
-    last_name: "",
-    email: "",
-    password: "",
-    password2: "",
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    password2: '',
   });
   const navigate = useNavigate();
 
@@ -42,154 +34,87 @@ function Signup({ setUserToken }) {
       email,
     };
 
-<<<<<<< HEAD
-    if (password === password2) {
-      setError(true);
-    }
-    try {
-      const { data } = await api.post("/register", user);
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("userProfile", JSON.stringify(data.userProfile));
-      setUserToken(data.token);
-      setRedirect(true);
-    } catch (error) {
-      setError(true);
-=======
     if (password !== password2) {
       return toast.error(`Passwords don't match`);
->>>>>>> d445ebef94cfb0d997fc4b007b4c3395fc5244b7
     }
     signUpUser(setUserToken, user);
   };
 
-<<<<<<< HEAD
-  // Redirect after login to quiz page
-  if (redirect) {
-    return <Navigate to="/quiz" />;
-=======
   if (localStorage.getItem('token')) {
     navigate('/quiz');
->>>>>>> d445ebef94cfb0d997fc4b007b4c3395fc5244b7
   }
 
   return (
-<<<<<<< HEAD
-    <div className="card">
-      {error && (
-        <Alert msg={"Please try registering again"} alertType={"danger"} />
-      )}
-      <div className="card-header">
-        {" "}
-        <h3>Signup</h3>{" "}
+    <div className='card'>
+      <div className='card-header'>
+        {' '}
+        <h3>Signup</h3>{' '}
       </div>
-      <div className="card-body">
+      <div className='card-body'>
         <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="firstName"> First Name</label>
+          <div className='form-group'>
+            <label htmlFor='firstName'> First Name</label>
             <input
-              className="form-control"
-              type="text"
-              name="first_name"
-              id="firstName"
+              className='form-control'
+              type='text'
+              name='first_name'
+              id='firstName'
               value={first_name}
               onChange={onChange}
             />
           </div>
-          <div className="form-group"> 
-            <label htmlFor="lastName"> Last Name</label>
-            <input className="form-control"
-              type="text"
-              name="last_name"
-              id="lastName"
+          <div className='form-group'>
+            <label htmlFor='lastName'> Last Name</label>
+            <input
+              className='form-control'
+              type='text'
+              name='last_name'
+              id='lastName'
               value={last_name}
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="email"> Email</label>
-            <input className="form-control"
-              type="email"
-              name="email"
-              id="Email"
+          <div className='form-group'>
+            <label htmlFor='email'> Email</label>
+            <input
+              className='form-control'
+              type='email'
+              name='email'
+              id='Email'
               value={email}
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="password1"> Password</label>
-            <input className="form-control"
-              type="password"
-              name="password"
-              id="password"
+          <div className='form-group'>
+            <label htmlFor='password1'> Password</label>
+            <input
+              className='form-control'
+              type='password'
+              name='password'
+              id='password'
               value={password}
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="password2">Confirm Password</label>
-            <input className="form-control"
-              type="password"
-              name="password2"
-              id="password2"
+          <div className='form-group'>
+            <label htmlFor='password2'>Confirm Password</label>
+            <input
+              className='form-control'
+              type='password'
+              name='password2'
+              id='password2'
               value={password2}
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
-            <button type="submit" className='btn btn-primary start-btn'>Login</button>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-primary start-btn'>
+              Login
+            </button>
           </div>
         </form>
       </div>
     </div>
-=======
-    <>
-      <div>Signup</div>
-      <form onSubmit={onSubmit}>
-        <label htmlFor='firstName'> First Name</label>
-        <input
-          type='text'
-          name='first_name'
-          id='firstName'
-          value={first_name}
-          onChange={onChange}
-        />
-        <label htmlFor='lastName'> Last Name</label>
-        <input
-          type='text'
-          name='last_name'
-          id='lastName'
-          value={last_name}
-          onChange={onChange}
-        />
-        <label htmlFor='email'> Email</label>
-        <input
-          type='email'
-          name='email'
-          id='Email'
-          value={email}
-          onChange={onChange}
-        />
-        <label htmlFor='password1'> Password</label>
-        <input
-          type='password'
-          name='password'
-          id='password'
-          value={password}
-          onChange={onChange}
-        />
-        <label htmlFor='password2'>Confirm Password</label>
-        <input
-          type='password'
-          name='password2'
-          id='password2'
-          value={password2}
-          onChange={onChange}
-        />
-
-        <button type='submit'>Login</button>
-      </form>
-    </>
->>>>>>> d445ebef94cfb0d997fc4b007b4c3395fc5244b7
   );
 }
 
