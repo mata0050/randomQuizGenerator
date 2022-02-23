3615ee44ea7cbbf4e141fb41e57ede9aa9ba8857
Ariel <90644357+Arie139@users.noreply.github.com>
2022-02-22 22:16:54 -0500

    added new package npm i in client added drop down menu for mobile view

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -7,6 +7,21 @@ import './Header.css';
 // Asset
 import logo from '../../images/logo.png';
 
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton
+} from '@chakra-ui/react'
+import { 
+  HamburgerIcon,
+  AddIcon,
+  ExternalLinkIcon,
+  RepeatIcon,
+  EditIcon
+} from '@chakra-ui/icons'
+
 function Header({ userToken, setUserToken }) {
   // logout and remove token from localStorage
   const logout = () => {
@@ -17,7 +32,53 @@ function Header({ userToken, setUserToken }) {
     <div className='fixed'>
       <div className='header'>
         <img src={logo} alt='Random Quiz' />
+        <Menu className='menuMobile'>
+          <MenuButton
+            as={IconButton}
+            aria-label='Options'
+            icon={<HamburgerIcon />}
+            variant='outline'
+          />
+          <MenuList>
+            <MenuItem>
+              <NavLink to='/signup'>
+                <span>Signup</span>
+              </NavLink>
+            </MenuItem>  
+            <MenuItem>
+              <NavLink to='/login'>
+                <span>Login</span>
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to='/'>
+                <span>Home</span>
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to='quiz'>
+                <span>Start Quiz</span>
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to='results'>
+                <span>Results</span>
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+            <NavLink to='leaderboard'>
+                <span>Leaderboard</span>
+              </NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to='!#' onClick={logout}>
+                <span className='button'>Logout</span>
+              </NavLink>
+            </MenuItem>
+          </MenuList>
+        </Menu>
         <div className='menu'>
+
           <NavLink to='/'>
             <span>Home</span>
           </NavLink>
