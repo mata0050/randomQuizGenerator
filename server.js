5745eb563eaf0755ec7cf17902f6c63d8fcdc12b
mata0050 <mata0050@outlook.com>
2022-02-23 10:31:43 -0500

    development ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
+const path = require('path');
 
 // Middleware
 app.use(express.json());
@@ -15,6 +16,17 @@ app.use('/api/youtube', require('./routes/api/youtube'));
 
 // Error Middleware
 app.use(errorHandler);
+
+// Serve static assets in production
+if (process.env.NPM_CONFIG_PRODUCTION) {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
